12c6c2354b94054ab6e6949b72bf7ac77cdf54d8
Mateo Munguia <juanpablomunguiagarcia@MacBook-Pro-de-Mateo-Munguia.local>
2023-02-13 13:51:03 -0400

    first commit

---
+++ Receiver/Controllers/EventController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.AspNetCore.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Dapr.Client;
+
+[ApiController]
+public class EventReceiverController : ControllerBase
+{
+    private readonly ILogger<EventReceiverController> _logger;
+    private readonly DaprClient _daprClient;
+
+    public EventReceiverController(ILogger<EventReceiverController> logger = null, DaprClient daprClient = null)
+    {
+        _logger = logger ?? NullLogger<EventReceiverController>.Instance;
+        _daprClient = daprClient ?? new DaprClientBuilder().Build();
+    }
+
+    [Route("subscribe")]
+    [HttpPost]
+    public async Task<IActionResult> Subscribe()
+    {
+        string value = await ReadRequestBodyAsString(Request);
+        _logger.LogInformation("Received event: {value}", value);
+        return Ok();
+    }
+
+    private async Task<string> ReadRequestBodyAsString(HttpRequest request)
+    {
+        using (var reader = new StreamReader(request.Body, Encoding.UTF8, true, 1024, true))
+        {
+            return await reader.ReadToEndAsync();
+        }
+    }
+}
+
+
+
+
---
+++ Sender/Controllers/EventController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Dapr.Client;
+using Google.Protobuf.WellKnownTypes;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using static Dapr.Client.Autogen.Grpc.v1.Dapr;
+using DaprClient = Dapr.Client.DaprClient;
+
+namespace EventPublisher.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+        private readonly ILogger<EventController> _logger;
+        public EventController(ILogger<EventController> logger = null)
+        {
+            _logger = logger ?? NullLogger<EventController>.Instance;
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> PublishEvent()
+        {
+            var httpClient = DaprClient.CreateInvokeHttpClient("myapps1");
+
+            //mensaje
+            var requestBody = "{\"data\":{\"message\":\"Hola Mundo desde Sender\"}}";
+
+            // Configura la solicitud POST
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri(httpClient.BaseAddress+ "subscribe"),
+                Content = new StringContent(requestBody, Encoding.UTF8, "application/json")
+            };
+            var value = request.RequestUri;
+            _logger.LogInformation("BaseAdress: {value}", value);
+            // Envía la solicitud
+            var response = await httpClient.SendAsync(request);
+
+            // Verifica si la solicitud fue exitosa
+            if (response.IsSuccessStatusCode)
+            {
+                return Ok("Evento publicado con éxito");
+            }
+            else
+            {
+                return BadRequest("Error al publicar evento");
+            }
+        }
+    }
+}
